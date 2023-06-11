995845cba844e364ff92a09170fc3226686d234c
a13xl <general01@tuta.io>
2023-06-11 21:32:54 +0200

    added help page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SummaryComponent } from './site/summary/summary.component';
+import { HelpComponent } from './site/help/help.component';
 
 const routes: Routes = [
   {path: '', component: SummaryComponent},
   {path: 'summary', component: SummaryComponent},
+  {path: 'help', component: HelpComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,13 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
+import { HelpComponent } from './site/help/help.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SummaryComponent,
+    HelpComponent,
   ],
   imports: [
     BrowserModule,
